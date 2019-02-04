63a17649fc493b409ad221af499f28ee16a917c4
Ваня Ясінський <ivanflash08@gmail.comgit config --global user.email ivanflash08@gmail.com>
2019-02-04 22:44:07 +0200

    release 0.1.0 04.02

--- js/editText.js
+++ js/editText.js
@@ -17,7 +17,7 @@ window.onload = function () {
         }
 
     }
-  ////////показать спрятать меню
+      ////////показать спрятать меню/////////////
     document.querySelector('.hide').onclick = function () {
       nav.style.display ='none';
         menu.style.opacity ='0';
@@ -33,5 +33,61 @@ window.onload = function () {
            menu.style.opacity ='1';
          }
        }
+
+       // выбор одного чекбокса////
+    inputs=document.getElementsByTagName("input");
+    for(var i=0; i<inputs.length; i++){
+      if(inputs[i].type=="checkbox"){
+         inputs[i].onchange=function(){
+            inputs=document.getElementsByTagName("input");
+              for(var i=0;i<inputs.length;i++){
+                if(inputs[i].type=="checkbox"){
+                  inputs[i].checked=false;
+                }
+                  this.checked=true;
+              }
+          }
+      }
+    }
+
+      // проверки чекбоксов//
+  let input = document.querySelector('#userText');
+  let len = document.querySelector('#symbol');
+  let smallText = document.querySelector('#small');
+  let large = document.querySelector('#large');
+  let everyOne = document.querySelector('#everyOne');
+  let res = document.querySelector('#resultReg');
+
+  input.onkeyup = function() {
+    transform();
+    // res.innerHTML = input.value;
+    len.innerHTML = "Общее количество символов: " + input.value.length;
+  }
+
+  function transform() {
+
+    if (smallText.checked == true) {
+      res.innerHTML = input.value.toLowerCase();
+    }
+
+    else if (large.checked == true) {
+      res.innerHTML = input.value.toUpperCase();
+    }
+
+    else if (everyOne.checked == true) {
+      // res.innerHTML = input.value;
+      for (var a = input.value , b = "", i = 0; i < a.length; i++) {
+        b += " " != a[i] ? i && " " != a[i - 1] ? a[i] : a[i].toUpperCase() : " ";
+      }
+      res.innerHTML = b;
+// console.log(b);
+    }
+    else {
+        res.innerHTML = input.value;
+    }
+
+
+}
+
 
 }
